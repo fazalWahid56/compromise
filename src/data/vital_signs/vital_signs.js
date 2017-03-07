0096ec414801f3ed43fc617ec2aeb609cf332169
FAZAL WAHID <FAZALWAHID@ISB.MTBC.COM>
2017-03-07 17:39:10 +0500

    vitals added

--- src/data/vital_signs/vital_signs.js
+++ src/data/vital_signs/vital_signs.js
@@ -1,8 +1,10 @@
-module.exports = [
-    'Temprature',
+module.exports = [    
+    'Temperature'
     'Heart rate', 
     'Respiratory rate',
+    'Respirations',
     'Blood pressure',
     'Height',
     'Weight', 
+    'Pulse',
 ]
