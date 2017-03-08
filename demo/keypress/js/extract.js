f97dcd9a81cc36fe2fae00a69fb91e546ad8363c
FAZAL WAHID <FAZALWAHID@ISB.MTBC.COM>
2017-03-08 14:13:37 +0500

    "Demo showing terms with clauses"

--- demo/keypress/js/extract.js
+++ demo/keypress/js/extract.js
@@ -1,24 +1,56 @@
 
 
-function ExtractData()
-{
+function ExtractData(){
     
     var text = $("#nlpText").val();
     if($.trim(text) !="")
     {
         var nlpJson={}
-        var nlpObj = nlp(text);
-        debugger;
-        nlpJson["Medications"]=nlpObj.medications().out('array');
-        nlpJson["Actions"]=nlpObj.actions().out("array");
-
-        nlpJson["Allergies"]=nlpObj.allergies().out("array");
-        nlpJson["Race"]=nlpObj.races().out("array");
-        nlpJson["Social History"]=nlpObj.social_histories().out("array");
-        nlpJson["Enthnicity"]=nlpObj.ethnicities().out("array");
-        nlpJson["Vitals"]=nlpObj.vital_signs().out("array");
-
+        var nlpObj = nlp(text.toLowerCase()).clauses();
+        nlpJson["Medications"]=getNlpData("Medications",nlpObj);
+        nlpJson["Actions"]=getNlpData("Actions",nlpObj);
+        nlpJson["Allergies"]=getNlpData("Allergies",nlpObj);
+        nlpJson["Race"]=getNlpData("Races",nlpObj);
+        nlpJson["Social History"]=getNlpData("Social_histories",nlpObj);
+        nlpJson["Enthnicity"]=getNlpData("Ethnicities",nlpObj);
+        nlpJson["Vitals"]=getNlpData("Vital_signs",nlpObj);        
         $('#json-renderer').jsonViewer(nlpJson);
     }
 
+}
+
+function getNlpData(tag,nlpObj){
+    var List =[]
+        jQuery.each( nlpObj.if('#'+tag).out('array'), function(index,value){  
+            var Obj={}
+            Obj["Clause"] = value;
+            switch (tag) { 
+	            case 'Medications': 
+		            Obj["Terms"]=nlp(value).medications().out('array');
+		            break;
+                case 'Actions': 
+		            Obj["Terms"]=nlp(value).actions().out('array');
+		            break;
+                case 'Allergies': 
+		            Obj["Terms"]=nlp(value).allergies().out('array');
+		            break;
+                case 'Races': 
+		            Obj["Terms"]=nlp(value).races().out('array');
+		            break;
+                case 'Social_histories': 
+		            Obj["Terms"]=nlp(value).social_histories().out('array');
+		            break;
+                case 'Ethnicities': 
+		            Obj["Terms"]=nlp(value).ethnicities().out('array');
+		            break;
+                case 'Vitals': 
+		            Obj["Terms"]=nlp(value).vital_signs().out('array');
+		            break;               
+                default:
+                    break;
+            }
+           
+            List.push(Obj);             
+        });
+    return List;
 }
